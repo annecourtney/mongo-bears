e37142d288398a1f5f6c83ed99ee02e6c209c1ee
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-04 16:13:51 -0500

    starting to work through bearcontroller

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,12 +1,32 @@
 const router = require('express').Router();
+const Bear = require('./bearModel'); // pull in our Bear model
 
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+    Bear.find()
+    .then(bears => {
+      res.status(202)
+      res.json({ bears })
+    })
+    .catch (err => {
+      res.status(200)
+      res.json({ message: 'Error fetching bears' });
+    })
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    const { species, latinName } = req.body;
+    const newBear = new Bear({ species, latinName });
+    newBear
+      .save()
+      .then(savedBear => {
+        res.status(201)
+        res.json({ savedBear })
+      })
+      .catch(err => {
+        res.status(201)
+        res.json({ status: 'please implement POST functionality' });
+    })
   });
 
 router
